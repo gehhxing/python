2991e668df3c61c7d9e88d9fcbb54e3c288a2e19
gehhxing <552096408@qq.com>
2018-07-22 22:43:24 +0800

    Add comment and replace the variable self.board in the method of the class Board

--- game_of_life/Board.py
+++ game_of_life/Board.py
@@ -1,12 +1,11 @@
 class Board:
     '''
-    版面类
-    包含版面初始化，版面元素下一个状态的计算和版面打印
+    Includes initialization and print of board,
+    and calculation of the next state of board
     '''
     def __init__(self, board=None, m=9, n=9):
         '''
-        初始化版面，以全0填充
-        需补充board和m,n冲突的处理
+        initialization of board
         '''
         if board:
             self.board = board
@@ -18,7 +17,10 @@ class Board:
             self.n = n
         self.next_board = board
 
-    def aroundNum(self, board, i, j):
+    def aroundNum(self, i, j):
+        '''
+        calculat the number of board[i][j]'s round
+        '''
         sum_j = []
         if i == 0:
             around_i = [0, 1, self.m - 1]
@@ -28,25 +30,30 @@ class Board:
             around_i = [i - 1, i, i + 1]
         for i in around_i:
             if j == 0:
-                sum_j.append(board[i][self.n - 1] + board[i][0] + board[i][1])
+                sum_j.append(self.board[i][self.n - 1] + self.board[i][0] + self.board[i][1])
             elif j == self.n - 1:
-                sum_j.append(board[i][self.n - 2] + board[i][self.n - 1] + board[i][0])
+                sum_j.append(self.board[i][self.n - 2] + self.board[i][self.n - 1] + self.board[i][0])
             else:
-                sum_j.append(board[i][j - 1] + board[i][j] + board[i][j + 1])
-        return sum(sum_j) - board[i][j]
+                sum_j.append(self.board[i][j - 1] + self.board[i][j] + self.board[i][j + 1])
+        return sum(sum_j) - self.board[i][j]
 
-    def nextState(self, board, i, j):
-        print(board[i][j])
-        num = self.aroundNum(board, i, j)
+    def nextState(self, i, j):
+        '''
+        calculat the next state of board
+        '''
+        num = self.aroundNum(i, j)
         if num in [0, 1, 4, 5, 6, 7, 8, 9]:
             self.next_board[i][j] = 0
         if num == 3:
             self.next_board[i][j] = 1
 
-    def printBoard(self, board):
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                self.nextState(board, i, j)
+    def printBoard(self):
+        '''
+        print board
+        '''
+        for i in range(len(self.board)):
+            for j in range(len(self.board[0])):
+                self.nextState(i, j)
         print(self.next_board)
 
 
@@ -70,15 +77,4 @@ if __name__ == '__main__':
              [0, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0]]
     board_a = Board(board)
-    board_a.printBoard(board)
-
-
-[0, 1, 0, 0, 0, 1, 1, 1, 0], 
-[0, 0, 1, 1, 0, 0, 0, 1, 0], 
-[0, 0, 1, 1, 0, 0, 1, 1, 0], 
-[0, 0, 0, 1, 0, 1, 0, 1, 0], 
-[0, 0, 0, 0, 1, 0, 1, 1, 0],
-[0, 1, 1, 0, 0, 0, 1, 0, 0], 
-[0, 0, 1, 1, 0, 1, 1, 1, 0],
-[0, 0, 1, 1, 1, 0, 0, 1, 0], 
-[0, 0, 1, 0, 1, 0, 0, 1, 1]
+    board_a.printBoard()
